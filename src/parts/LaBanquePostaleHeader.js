3f19ad63a472777c396dec51655871a334a88014
Nicolas Bouillon <nicolas@bouil.org>
2013-05-13 09:24:07 +0200

    Bump version when fixing userscripts.org update issue

--- src/parts/LaBanquePostaleHeader.js
+++ src/parts/LaBanquePostaleHeader.js
@@ -3,7 +3,7 @@
 // @namespace     org.bouil
 // @description   Remove virtual keyboard and add a classic input text field for the password on La Banque Postale website https://www.labanquepostale.fr/
 // @include       https://voscomptesenligne.labanquepostale.fr/wsost/OstBrokerWeb/loginform?TAM_OP=login&ERROR_CODE=0x00000000&URL=%2Fvoscomptes%2FcanalXHTML%2Fidentif.ea%3Forigin%3Dparticuliers
-// @version       0.2
+// @version       0.3
 // @updateURL     http://userscripts.org/scripts/source/94023.user.js
 // @grant         none
 // ==/UserScript==
