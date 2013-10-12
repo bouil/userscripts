b2aaf32e91eeee234fb1ea4088653b30bff75df7
Nicolas Bouillon <nicolas@bouil.org>
2013-10-12 15:22:06 +0200

    Update  BNP Parisbas script for the new version of website.

--- src/parts/BnpParibasHeader.js
+++ src/parts/BnpParibasHeader.js
@@ -4,8 +4,11 @@
 // @description   Remove virtual keyboard and add a classic input text field for the password on BNP Paribas website https://www.secure.bnpparibas.net
 // @include       https://www.secure.bnpparibas.net/banque/portail/particulier/HomeConnexion*
 // @include       http://localhost:5789/tests/bnp/Tous%20les%20produits%20et%20services%20de%20votre%20banque%20en%20France.html
-// @version       1.3.4
+// @version       1.4.0
 // @updateURL     http://userscripts.org/scripts/source/158001.user.js
-// @require  https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-// @grant         none
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @resource      bootstrapcss https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
+// @grant         GM_addStyle
+// @grant         GM_info
+// @grant         GM_getResourceText
 // ==/UserScript==
