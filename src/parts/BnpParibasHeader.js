e7bedebcc893031d4e15e1ea3fa03820181e44b6
Nicolas Bouillon <nicolas@bouil.org>
2014-06-22 08:33:55 +0200

    New version and update URL for BNP (openuserjs.org)x

--- src/parts/BnpParibasHeader.js
+++ src/parts/BnpParibasHeader.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name          BNP Paribas - No Virtual Keyboard
 // @namespace     org.bouil
+// @author        bouil
+// @copyright     2013-2014, https://github.com/bouil/userscripts
 // @description   Remove virtual keyboard and add a classic input text field for the password on BNP Paribas website https://www.secure.bnpparibas.net
 // @include       https://www.secure.bnpparibas.net/banque/portail/particulier/HomeConnexion*
 // @include       http://localhost:5789/tests/bnp/Tous%20les%20produits%20et%20services%20de%20votre%20banque%20en%20France.html
-// @version       1.4.0
-// @updateURL     http://userscripts.org/scripts/source/158001.user.js
+// @version       1.4.1
+// @updateURL     https://openuserjs.org/install/bouil/BNP_Paribas_-_No_Virtual_Keyboard.user.js
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @resource      bootstrapcss https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
 // @grant         GM_addStyle
