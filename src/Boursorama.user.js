3a4419dab35fe786758e3c16f8fc7c82d370f7cc
Nicolas Bouillon <nicolas@bouil.org>
2014-10-24 21:32:21 +0200

    Add first version for Boursorama

--- src/Boursorama.user.js
+++ src/Boursorama.user.js
@@ -1,37 +1,33 @@
-//#include "parts/BnpParibasHeader.js"
+//#include "parts/BoursoramaHeader.js"
 //#include "parts/Md5.js"
 
-var scriptName= GM_info.script.name
+
+var scriptName = GM_info.script.name
 var version = GM_info.script.version;
 var debug = false;
 
 var md5ToNumber = new Object();
 // firefox
-md5ToNumber["f3543cedaee50789fc8ce978ce402399"] = -1;
-md5ToNumber["23c03b703a8a817b1a314c9fb80cb7fa"] = 0;
-md5ToNumber["744f74197d3a9526c04259bd058f278f"] = 1;
-md5ToNumber["527a26b94f74de72e4b630313e518d59"] = 2;
-md5ToNumber["4b37641fbeebe7d0cb7d6a9725ec07b0"] = 3;
-md5ToNumber["ae9f29c4db8f33224f525242c45db607"] = 4;
-md5ToNumber["ae9f29c4db8f33224f525242c45db607"] = 4;
-md5ToNumber["b9268ee64a09cfb95f756c1850959b1f"] = 5;
-md5ToNumber["4f521ab60c7dccb31a4df734cc1a01ba"] = 6;
-md5ToNumber["5bb3a6c80cbf9aa379ca2b64b6e379b7"] = 7;
-md5ToNumber["9fa178bbb2c86711f7f6a537e235ef2d"] = 8;
-md5ToNumber["390f9e42e02fe5e91f2384ab25b24f4b"] = 9;
+md5ToNumber["aa638e434ef27fc73aaa2b812c6d6940"] = -1;
+md5ToNumber["f7eff870c6bc8a211438887feecf02ba"] = 0;
 
-// chrome
-md5ToNumber["83a3102b20ac27ecfb56f58cc81db2b3"] = -1;
-md5ToNumber["0e5e939030233b16142d780f851e1d17"] = 0;
-md5ToNumber["aceef4b573b173f1a8ba71832b4cbd03"] = 1;
-md5ToNumber["4295a72207c26072623486a0fc730e9f"] = 2;
-md5ToNumber["9f69717913d583231d7edd7f35b546cd"] = 3;
-md5ToNumber["edefdb0e05735bd295d31c9521108e7b"] = 4;
-md5ToNumber["2e4d57f80e298758f2bf717c22c9b7e7"] = 5;
-md5ToNumber["4f47364e582099205efad7b6465af962"] = 6;
-md5ToNumber["5b1c2ce646ea54c438cf23c21732ae17"] = 7;
-md5ToNumber["a37439d81d6545237656aa096faa80d4"] = 8;
-md5ToNumber["4dbad727dec54840449bdf9fd38c2fa6"] = 9;
+md5ToNumber["a3f2a856c6d100e180be2f52a331d23e"] = 1;
+md5ToNumber["5e4c5d26750e9360dd0a674e1233b12c"] = 1;
+
+md5ToNumber["859cf20afa3d16d35ca3bd96fdde8799"] = 2;
+md5ToNumber["c9a5b1f4f63bd97bb024383a9f21ff75"] = 2;
+
+md5ToNumber["735b7b06bd6e2bad29da18126b5080a7"] = 3;
+md5ToNumber["6b0223be3d4c74c95316363d49c53be2"] = 4;
+
+md5ToNumber["e589b3fbdfed390e108322d7a1d0ceca"] = 5;
+md5ToNumber["44fc31d0f093a6b41636b2ce7fd97cd8"] = 5;
+
+md5ToNumber["b4a14503a39e9b448f3c6dd0171018c8"] = 6;
+md5ToNumber["87b9a2a3958615122657a3a05b7088b0"] = 7;
+md5ToNumber["38cb00677543be37b86d1ad242e714aa"] = 8;
+md5ToNumber["e3f02c152a49876d759e74e40c71774c"] = 9;
+
 
 var number2GridPosition;
 
@@ -50,23 +46,18 @@ function getNumberFromImgMd5(imageDataBase64) {
  * Taken from http://userscripts.org/scripts/show/126488 - FreeMobile TinyAuth
  */
 function convertColor(image_data) {
-    for (var x = 0; x < image_data.width; x++) {
-        for (var y = 0; y < image_data.height; y++) {
-            var i = x * 4 + y * 4 * image_data.width;
-            var luma = Math.floor(image_data.data[i] * 299 / 1000 + image_data.data[i + 1] * 587 / 1000 +
-                                      image_data.data[i + 2] * 114 / 1000);
-
-            image_data.data[i] = luma;
-            image_data.data[i + 1] = luma;
-            image_data.data[i + 2] = luma;
-            image_data.data[i + 3] = 255;
-            if (image_data.data[i] > 200 || image_data.data[i + 3] == 0) {
-                image_data.data[i] = 255;
-                image_data.data[i + 1] = 255;
-                image_data.data[i + 2] = 255;
-                image_data.data[i + 3] = 0;
-            }
+    var pix = image_data.data;
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+        var grayscale = pix[i  ] * .3 + pix[i + 1] * .59 + pix[i + 2] * .11;
+        if (grayscale > 100) {
+            grayscale = 255;
+        } else {
+            grayscale = 0;
         }
+        pix[i  ] = grayscale; 	// red
+        pix[i + 1] = grayscale; 	// green
+        pix[i + 2] = grayscale; 	// blue
+        // alpha
     }
 };
 
@@ -77,10 +68,10 @@ function decodeGrid(gridImgSrc) {
 
     number2GridPosition = new Object();
 
-    var nbCols = 5;
-    var nbRows = 5;
-    var numberPartWidth = 26;
-    var numberPartHeight = 26;
+    var nbCols = 3;
+    var nbRows = 4;
+    var numberPartWidth = 98;
+    var numberPartHeight = 58;
     for (y = 1; y <= nbRows; y++) { // each row
         for (x = 1; x <= nbCols; x++) { // each col
             var gridPosition = (((y - 1) * nbCols) + x);
@@ -93,11 +84,11 @@ function decodeGrid(gridImgSrc) {
 
             ctx.fillStyle = "rgb(255,255,100)";
             ctx.fillRect(0, 0, numberPartWidth, numberPartHeight);
-            // chaque case chiffre fait 26px*26px sans la bordure de 1px
+            // chaque case chiffre fait 98px*58 sans la bordure de 1px
 
-            var xoffset = x;
+            var xoffset = 1 + (x - 1) * 2;
             var sx = xoffset + (numberPartWidth * (x - 1)); // The x coordinate where to start clipping
-            var yoffset = y;
+            var yoffset = 1 + (y - 1) * 2;
             var sy = yoffset + (numberPartHeight * (y - 1)); // The y coordinate where to start clipping
             var swidth = numberPartWidth; // The width of the clipped image
             var sheight = numberPartHeight; // The height of the clipped image
@@ -163,72 +154,46 @@ function submitGrid($grid) {
         return;
     }
 
+    var area = $("map area");
+
     var password = $("#gm_password").val();
-    var $targetPasswordField = $("form[name=logincanalnetbis]").find("input[name=ch5temp]");
-    var $targetPasswordStarField = $("form[name=logincanalnetbis]").find("input[name=ch2]");
     for (s = 0; s < password.length; s++) {
         var grilleChar = number2GridPosition[password[s]];
-        if (grilleChar < 10) {
-            grilleChar = "0" + grilleChar;
-        }
-        if (debug){
+        if (debug) {
             console.log(grilleChar);
         }
-        $targetPasswordField.val($targetPasswordField.val() + grilleChar);
-        $targetPasswordStarField.val($targetPasswordStarField.val() + "*");
+
+        var toExec = $(area[grilleChar - 1]).attr("onclick");
+        if (debug) {
+            console.log("eval(" + toExec + ")");
+        }
+        eval(toExec);
 
     }
 
     if (!debug) {
-        var $formToSubmit = $("form[name=logincanalnet]");
-        var $passwordToSubmit = $formToSubmit.find("input[name=ch5]");
-        $passwordToSubmit.val($targetPasswordField.val());
+        var $formToSubmit = $("form#identification_client");
         $formToSubmit.get(0).submit();
     } else {
         console.log("Debug mode: no submit");
     }
 };
 
-function addLoginInput($form) {
-    // add a new login input
-
-    var $divControlGroupLogin = $(_("div")).addClass("control-group");
-    var $labelLogin = $(_("label")).addClass("control-label").attr("for", "ch1").text("Identifiant");
-    $divControlGroupLogin.append($labelLogin);
-    var $divControlLoginInput = $(_("div")).addClass("controls");
-
-    var $login = $form.find("input[name=ch1]").attr("placeholder", "identifiant");
-    $newInputLogin = $login.clone();
-    $login.attr("name", "oldCh1");
-
-    // remove the old login input
-    var $tableLogin = $form.find(":last-child");
-    $tableLogin.hide();
-
-    $newInputLogin.appendTo($divControlLoginInput);
-
-    $divControlGroupLogin.append($divControlLoginInput);
-
-    $form.append($divControlGroupLogin);
-    // put caret in login input
-    $newInputLogin.focus();
-}
-
 function addPasswordInput($form) {
     // add a password input
 
-    var $divControlGroupPassword = $(_("div")).addClass("control-group");
-    var $labelPassword = $(_("label")).addClass("control-label").attr("for", "gm_password").text("Mot de passe");
+    var $divControlGroupPassword = $(_("div"));
+    var $labelPassword = $(_("label")).attr("for", "gm_password").text("Mot de passe");
     $divControlGroupPassword.append($labelPassword);
-    var $divControlPasswordInput = $(_("div")).addClass("controls");
+    var $divControlPasswordInput = $(_("div"));
     var $newInputPassword = $(_("input")).attr("type", "password").attr("name", "gm_password").attr("id",
-                                                                                                    "gm_password").attr("autocomplete",
-                                                                                                                        "On").attr("maxlength",
-                                                                                                                                   "6").attr("placeholder",
-                                                                                                                                             "mot de passe");
+        "gm_password").attr("autocomplete",
+        "On").attr("maxlength",
+        "6").attr("placeholder",
+        "mot de passe");
     $newInputPassword.appendTo($divControlPasswordInput);
     $divControlGroupPassword.append($divControlPasswordInput);
-    $form.append($divControlGroupPassword);
+    $form.find("label#login-password-label").before($divControlGroupPassword);
 }
 
 function addSubmitButton($form, $grid) {
@@ -261,8 +226,6 @@ function customizeUi($form, $grid) {
 
     $form.addClass("form-horizontal");
 
-    addLoginInput($form);
-
     addPasswordInput($form);
 
     addSubmitButton($form, $grid);
@@ -270,13 +233,15 @@ function customizeUi($form, $grid) {
     addScriptInfos($form);
 
     if (!debug) {
-        $("table.identification div.rubrique").hide();
+        $("#login-password-label").hide();
+        $("#login-password-label").next().hide();
+        $("#b3fdea3").hide();
     }
 }
 
 function main() {
-    var $form = $("form[name=logincanalnet]");
-    var $grid = $("#secret-nbr-keyboard");
+    var $form = $("form#identification_client");
+    var $grid = $("#login-pad img");
 
     if (!$grid || ($grid.length == 0)) {
         alert("Aucune grille d'identification trouvee")
@@ -289,12 +254,12 @@ function main() {
         console.log($grid.get()[0]);
     }
 
-    gridImgSrc = $grid.css('background-image').replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
-    if (debug){
+    gridImgSrc = $grid.attr("src");
+    if (debug) {
         console.log("Grid image = " + gridImgSrc);
     }
 
-    $('<img/>').attr('src', gridImgSrc).load(function() {
+    $('<img/>').attr('src', gridImgSrc).load(function () {
         $(this).remove(); // prevent memory leaks
         decodeGrid(gridImgSrc);
         customizeUi($form, $grid);
---
+++ src/parts/BoursoramaHeader.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name          Boursorama - No Virtual Keyboard
+// @namespace     org.bouil
+// @author        bouil
+// @copyright     2013-2014, https://github.com/bouil/userscripts
+// @description   Remove virtual keyboard and add a classic input text field for the password on Boursorama website https://www.boursorama.com/connexion.phtml. NOT compatible with Chrome yet.
+// @include       https://www.boursorama.com/connexion.phtml
+// @include       http://localhost:63342/userscripts/tests/boursorama/Connexion.html
+// @version       0.1
+// @updateURL     https://openuserjs.org/install/bouil/Boursorama_-_No_Virtual_Keyboard.user.js
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @resource      bootstrapcss https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
+// @grant         GM_addStyle
+// @grant         GM_info
+// @grant         GM_getResourceText
+// ==/UserScript==
