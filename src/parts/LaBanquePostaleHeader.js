5384034e075b394cae827a1810f352cff987a43c
Nicolas Bouillon <nicolas@bouil.org>
2023-02-05 13:16:10 +0100

    Adjust header for openuserjs.org

--- src/parts/LaBanquePostaleHeader.js
+++ src/parts/LaBanquePostaleHeader.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name          La Banque Postale - No Virtual Keyboard
+// @license       MIT
 // @namespace     org.bouil
 // @author        bouil
 // @collaborator  foudfou
-// @copyright     2013-2016, https://github.com/bouil/userscripts
+// @copyright     2013-2023, https://github.com/bouil/userscripts
 // @description   Remove virtual keyboard and add a classic input text field for the password on La Banque Postale website https://www.labanquepostale.fr/
 // @include       https://voscomptesenligne.labanquepostale.fr/wsost/OstBrokerWeb/loginform?TAM_OP=login&ERROR_CODE=0x00000000&URL=%2Fvoscomptes%2FcanalXHTML%2Fidentif.ea%3Forigin%3Dparticuliers
 // @version       0.4.9
 // @updateURL     https://openuserjs.org/install/bouil/La_Banque_Postale_-_No_Virtual_Keyboard.user.js
-// @grant         GM_info
-// @grant         GM_getMetadata
+// @oujs:author   bouil
+// @oujs:collaborator foudfou
 // ==/UserScript==
