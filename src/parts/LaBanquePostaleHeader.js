e5ab9aedfe32d122e0fe2f867e79a31dbc4018f7
bouil <nicolas@bouil.org>
2023-02-23 10:46:38 +0100

    LaBanquePostale: bump version number

--- src/parts/LaBanquePostaleHeader.js
+++ src/parts/LaBanquePostaleHeader.js
@@ -7,7 +7,7 @@
 // @copyright     2013-2023, https://github.com/bouil/userscripts
 // @description   Remove virtual keyboard and add a classic input text field for the password on La Banque Postale website https://www.labanquepostale.fr/
 // @include       https://voscomptesenligne.labanquepostale.fr/wsost/OstBrokerWeb/loginform?TAM_OP=login&ERROR_CODE=0x00000000&URL=%2Fvoscomptes%2FcanalXHTML%2Fidentif.ea%3Forigin%3Dparticuliers
-// @version       0.4.9
+// @version       0.5.0
 // @updateURL     https://openuserjs.org/install/bouil/La_Banque_Postale_-_No_Virtual_Keyboard.user.js
 // @oujs:author   bouil
 // @oujs:collaborator foudfou
